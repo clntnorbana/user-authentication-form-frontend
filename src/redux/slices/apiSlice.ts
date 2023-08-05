f08c53a62fb550f4c1cd59c05d116f9b8cfe316b
clinton otbana <rbnclntn@gmail.com>
2023-08-05 14:17:30 +0800

    cors configure

--- src/redux/slices/apiSlice.ts
+++ src/redux/slices/apiSlice.ts
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// const BASE_URL = "https://user-authentication-server.onrender.com";
+
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://user-authentication-server.onrender.com",
+  baseUrl: "",
 });
 
 export const apiSlice = createApi({
