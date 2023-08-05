11e5e7fdeaeb082cc854ba423f9fe51e3e5ea5e0
clinton otbana <rbnclntn@gmail.com>
2023-08-05 13:49:26 +0800

    commit

--- src/redux/slices/apiSlice.ts
+++ src/redux/slices/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "",
+  baseUrl: "https://user-authentication-server.onrender.com",
 });
 
 export const apiSlice = createApi({
